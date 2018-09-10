ed97898a51ce205023c248fe3fb9332a17c62823
Al Madireddy <al.madireddy@gmail.com>
2018-09-10 16:21:09 -0500

    uninstalled yarn

--- app/routes/archive.js
+++ app/routes/archive.js
@@ -1,8 +1,8 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
 
 
-export default Ember.Route.extend({
+export default Route.extend({
   model() {
     return hash({
       photos: this.store.findAll('photo'),
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
 
-export default Ember.Route.extend({
+export default Route.extend({
   model() {
     return hash({
       photos: this.store.findAll('photo'),
