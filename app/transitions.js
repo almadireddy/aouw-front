df94c6a71524b3c24e73f7a96b05ac7c83c49e24
Al Madireddy <al.madireddy@gmail.com>
2018-09-13 13:16:28 -0500

    lazy loading images

--- app/transitions.js
+++ app/transitions.js
@@ -1,5 +1,12 @@
 export default function(){
   this.transition(
+    this.hasClass('liquid-dialog-container'),
     this.use('fade', {duration: 500})
+  );
+  this.transition(
+    this.fromRoute('index'),
+    this.toRoute('archive'),
+    this.use('toLeft'),
+    this.reverse('toRight')
   );
 }
