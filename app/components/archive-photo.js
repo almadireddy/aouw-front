4271c3a657a671edccc8b6240e5aec810cc2b51f
Al Madireddy <al.madireddy@gmail.com>
2018-09-12 20:52:36 -0500

    picture and quote show in modal, added animation dependencies

--- app/components/archive-photo.js
+++ app/components/archive-photo.js
@@ -2,26 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   quote: null,
-  showQuote: false,
-  classNameBindings: ['showQuote'],
 
   init() {
     this._super(...arguments);
     let literature = this.get('literature');
     let rand = Math.random() * literature.content.length;
     
-    this.set('quote', literature.content[Math.floor(rand)]);
+    this.set('quote', literature.content[Math.floor(rand)].__data.quote);
   },
 
   actions: {
     imageClicked(e) {
-      if (!this.showQuote) {
-        e.target.lastChild.innerText = this.get('quote').__data.quote;
-        this.set('showQuote', true);
-      }
-      else {
-        this.set('showQuote', false);
-      }
+      this.sendAction('show', this.get('quote'), this.get('photo').get('nypl'));
     }
   }
 });
--- app/controllers/archive.js
+++ app/controllers/archive.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isShowingModal: false,
+  quote: null,
+  imgSrc: null,
+
+  actions: {
+    showPictureModal(quote, nypl) {
+      this.set('imgSrc', 'https://images.nypl.org/index.php?id='+ nypl +'&t=w')
+      this.set('quote', quote)
+      this.set('isShowingModal', true)
+    },
+
+    toggleModal() {
+      this.toggleProperty('isShowingModal')
+    }
+  }
 });
