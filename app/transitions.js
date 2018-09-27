361c3cdfedec8402811d1192f33bfa453ad7ead1
Al Madireddy <al.madireddy@gmail.com>
2018-09-27 13:44:07 -0500

    font and other changes before print test

--- app/transitions.js
+++ app/transitions.js
@@ -3,10 +3,4 @@ export default function(){
     this.hasClass('liquid-dialog-container'),
     this.use('fade', {duration: 500})
   );
-  this.transition(
-    this.fromRoute('index'),
-    this.toRoute('archive'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
 }
