3da1da8b6b41b8b928a2f153542017366685fae1
Al Madireddy <al.madireddy@gmail.com>
2018-10-11 15:14:15 -0500

    adding dist

---
+++ dist/assets/tests.js
@@ -0,0 +1,549 @@
+'use strict';
+
+define('archive-of-unnamed-women/tests/app.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | app');
+
+  QUnit.test('adapters/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'adapters/application.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/archive-photo.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/archive-photo.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/click-to-show.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/click-to-show.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/nav-bar.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/nav-bar.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('components/site-footer.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'components/site-footer.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('controllers/archive.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/archive.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('controllers/index.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/index.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('models/collection.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/collection.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('models/custom-inflector-rules.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/custom-inflector-rules.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('models/literature.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/literature.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('models/photo.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/photo.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/archive.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/archive.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/credits.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/credits.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/index.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/index.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('serializers/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'serializers/application.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('transitions.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'transitions.js should pass ESLint\n\n');
+  });
+});
+define('archive-of-unnamed-women/tests/helpers/destroy-app', ['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = destroyApp;
+  function destroyApp(application) {
+    Ember.run(application, 'destroy');
+  }
+});
+define('archive-of-unnamed-women/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'archive-of-unnamed-women/tests/helpers/start-app', 'archive-of-unnamed-women/tests/helpers/destroy-app'], function (exports, _qunit, _startApp, _destroyApp) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  exports.default = function (name) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _startApp.default)();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return Promise.resolve(afterEach).then(function () {
+          return (0, _destroyApp.default)(_this.application);
+        });
+      }
+    });
+  };
+
+  var Promise = Ember.RSVP.Promise;
+});
+define('archive-of-unnamed-women/tests/helpers/resolver', ['exports', 'archive-of-unnamed-women/resolver', 'archive-of-unnamed-women/config/environment'], function (exports, _resolver, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+
+  var resolver = _resolver.default.create();
+
+  resolver.namespace = {
+    modulePrefix: _environment.default.modulePrefix,
+    podModulePrefix: _environment.default.podModulePrefix
+  };
+
+  exports.default = resolver;
+});
+define('archive-of-unnamed-women/tests/helpers/start-app', ['exports', 'archive-of-unnamed-women/app', 'archive-of-unnamed-women/config/environment'], function (exports, _app, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = startApp;
+  function startApp(attrs) {
+    var attributes = Ember.merge({}, _environment.default.APP);
+    attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+
+    return Ember.run(function () {
+      var application = _app.default.create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+      return application;
+    });
+  }
+});
+define('archive-of-unnamed-women/tests/integration/components/archive-photo-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForComponent)('archive-photo', 'Integration | Component | archive photo', {
+    integration: true
+  });
+
+  (0, _emberQunit.test)('it renders', function (assert) {
+
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+
+    this.render(Ember.HTMLBars.template({
+      "id": "ySN9ZMXj",
+      "block": "{\"statements\":[[1,[26,[\"archive-photo\"]],false]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), '');
+
+    // Template block usage:
+    this.render(Ember.HTMLBars.template({
+      "id": "DkNPN61z",
+      "block": "{\"statements\":[[0,\"\\n\"],[6,[\"archive-photo\"],null,null,{\"statements\":[[0,\"      template block text\\n\"]],\"locals\":[]},null],[0,\"  \"]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), 'template block text');
+  });
+});
+define('archive-of-unnamed-women/tests/integration/components/click-to-show-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForComponent)('click-to-show', 'Integration | Component | click to show', {
+    integration: true
+  });
+
+  (0, _emberQunit.test)('it renders', function (assert) {
+
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+
+    this.render(Ember.HTMLBars.template({
+      "id": "4hvt3oAo",
+      "block": "{\"statements\":[[1,[26,[\"click-to-show\"]],false]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), '');
+
+    // Template block usage:
+    this.render(Ember.HTMLBars.template({
+      "id": "zEc/hcNM",
+      "block": "{\"statements\":[[0,\"\\n\"],[6,[\"click-to-show\"],null,null,{\"statements\":[[0,\"      template block text\\n\"]],\"locals\":[]},null],[0,\"  \"]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), 'template block text');
+  });
+});
+define('archive-of-unnamed-women/tests/integration/components/nav-bar-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForComponent)('nav-bar', 'Integration | Component | nav bar', {
+    integration: true
+  });
+
+  (0, _emberQunit.test)('it renders', function (assert) {
+
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+
+    this.render(Ember.HTMLBars.template({
+      "id": "ntAlUnVm",
+      "block": "{\"statements\":[[1,[26,[\"nav-bar\"]],false]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), '');
+
+    // Template block usage:
+    this.render(Ember.HTMLBars.template({
+      "id": "UKwhv7kh",
+      "block": "{\"statements\":[[0,\"\\n\"],[6,[\"nav-bar\"],null,null,{\"statements\":[[0,\"      template block text\\n\"]],\"locals\":[]},null],[0,\"  \"]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), 'template block text');
+  });
+});
+define('archive-of-unnamed-women/tests/integration/components/site-footer-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForComponent)('site-footer', 'Integration | Component | site footer', {
+    integration: true
+  });
+
+  (0, _emberQunit.test)('it renders', function (assert) {
+
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+
+    this.render(Ember.HTMLBars.template({
+      "id": "V1koaGwl",
+      "block": "{\"statements\":[[1,[26,[\"site-footer\"]],false]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), '');
+
+    // Template block usage:
+    this.render(Ember.HTMLBars.template({
+      "id": "S5KO+4Iz",
+      "block": "{\"statements\":[[0,\"\\n\"],[6,[\"site-footer\"],null,null,{\"statements\":[[0,\"      template block text\\n\"]],\"locals\":[]},null],[0,\"  \"]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}",
+      "meta": {}
+    }));
+
+    assert.equal(this.$().text().trim(), 'template block text');
+  });
+});
+define('archive-of-unnamed-women/tests/test-helper', ['archive-of-unnamed-women/tests/helpers/resolver', 'ember-qunit', 'ember-cli-qunit'], function (_resolver, _emberQunit, _emberCliQunit) {
+  'use strict';
+
+  (0, _emberQunit.setResolver)(_resolver.default);
+  (0, _emberCliQunit.start)();
+});
+define('archive-of-unnamed-women/tests/tests.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | tests');
+
+  QUnit.test('helpers/destroy-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/module-for-acceptance.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('helpers/start-app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('integration/components/archive-photo-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'integration/components/archive-photo-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('integration/components/click-to-show-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'integration/components/click-to-show-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('integration/components/nav-bar-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'integration/components/nav-bar-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('integration/components/site-footer-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'integration/components/site-footer-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/adapters/application-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/adapters/application-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/controllers/archive-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/controllers/archive-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/controllers/index-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/controllers/index-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/models/collection-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/models/collection-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/models/literature-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/models/literature-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/models/photo-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/models/photo-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/routes/archive-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/archive-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/routes/credits-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/credits-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/routes/index-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/index-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/serializers/application-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/serializers/application-test.js should pass ESLint\n\n');
+  });
+});
+define('archive-of-unnamed-women/tests/unit/adapters/application-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('adapter:application', 'Unit | Adapter | application', {
+    // Specify the other units that are required for this test.
+    // needs: ['serializer:foo']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var adapter = this.subject();
+    assert.ok(adapter);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/controllers/archive-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('controller:archive', 'Unit | Controller | archive', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var controller = this.subject();
+    assert.ok(controller);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/controllers/index-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('controller:index', 'Unit | Controller | index', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var controller = this.subject();
+    assert.ok(controller);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/models/collection-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForModel)('collection', 'Unit | Model | collection', {
+    // Specify the other units that are required for this test.
+    needs: []
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var model = this.subject();
+    // let store = this.store();
+    assert.ok(!!model);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/models/literature-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForModel)('literature', 'Unit | Model | literature', {
+    // Specify the other units that are required for this test.
+    needs: []
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var model = this.subject();
+    // let store = this.store();
+    assert.ok(!!model);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/models/photo-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForModel)('photo', 'Unit | Model | photo', {
+    // Specify the other units that are required for this test.
+    needs: []
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var model = this.subject();
+    // let store = this.store();
+    assert.ok(!!model);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/routes/archive-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:archive', 'Unit | Route | archive', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/routes/credits-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:credits', 'Unit | Route | credits', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/routes/index-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleFor)('route:index', 'Unit | Route | index', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('archive-of-unnamed-women/tests/unit/serializers/application-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForModel)('application', 'Unit | Serializer | application', {
+    // Specify the other units that are required for this test.
+    needs: ['serializer:application']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it serializes records', function (assert) {
+    var record = this.subject();
+
+    var serializedRecord = record.serialize();
+
+    assert.ok(serializedRecord);
+  });
+});
+require('archive-of-unnamed-women/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
